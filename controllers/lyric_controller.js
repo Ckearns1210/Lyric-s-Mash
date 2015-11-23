dfb0192e765c855d2ae86716cc71f853cde035b0
jcjohnny <jc2johnny@gmail.com>
2015-11-23 14:20:52 -0500

    parsed through

--- controllers/lyric_controller.js
+++ controllers/lyric_controller.js
@@ -9,14 +9,21 @@ var trySendData = function(item,res){
     item ? res.send(item) : res.sendStatus(404).end();
 };
 
-
 router.get('/lyric/:search', function(req,res){
     console.log("im here");
 
-    request("http://api.lyricsnmusic.com/songs?api_key=53f1395dbd8adff15b086199e1c500&lyrics=" + encodeURI(req.params.search), function (error, response, body) {
+    request("http://api.musixmatch.com/ws/1.1/track.search?f_has_lyrics=1&page=1&page_size=15&q_lyrics="+ encodeURI(req.params.search) +"&s_track_rating=desc&apikey=032f5b65b1a0deac1f5f44afc9d548c0", function (error, response, body) {
         trySendData(body, res);
     });
 });
+
+// router.get('/lyric/:search', function(req,res){
+//     console.log("im here");
+//
+//     request("http://api.lyricsnmusic.com/songs?api_key=53f1395dbd8adff15b086199e1c500&lyrics=" + encodeURI(req.params.search), function (error, response, body) {
+//         trySendData(body, res);
+//     });
+// });
 
 module.exports = router;
 
--- public/js/app.js
+++ public/js/app.js
@@ -5,50 +5,20 @@ $(function(){
         e.preventDefault();
         var searchTerm1 = $('#input-4').val();
         $.get(('/lyric/' + searchTerm1), render);
-        var searchTerm2 = $('#input-5').val();
-        $.get(('/lyric/' + searchTerm2), render2);
-        var searchTerm3 = $('#input-6').val();
-        $.get(('/lyric/' + searchTerm2), render3);
+
     });
 
-    var renderTemplate_showMany = Handlebars.compile( $('template#show-many').html());
 
-var render = function(data){
+var render = function(data) {
     var parser = JSON.parse(data);
-    console.log(parser);
+    var songList = parser.message.body.track_list;
+    // console.log(songList[0]);
+    var random = Math.floor(Math.random() * (songList.length + 1));
+    // var $lyric1 = $('<p>').text( JSON.stringify(songList[random]));
+    console.log(songList[random].track.track_id);
 
-    // //combining works!
-    // captured = _.extend($('#input-4').val(''), $('#input-5').val(''));
-    //
-    // //feed the template
-    // var html = renderTemplate_showMany({lyric:parser});
-    // $('#show-lyrics').html('').show().append(html);
 
-    //cant get random of each element with random using template... can't figure it out
-    //will do it with appending
-    var rando = Math.floor(Math.random() * (parser.length + 1));
-    var $lyric1 = $('<p>').text( JSON.stringify(parser[rando].snippet).replace(/[^A-Za-z0-9]/g, ' '));
-    $('#show-lyrics').append($lyric1);
 };
-
-var render2 = function(data){
-    var parser = JSON.parse(data);
-    console.log(parser);
-
-    var rando = Math.floor(Math.random() * (parser.length + 1));
-    var $lyric2 = $('<p>').text( JSON.stringify(parser[rando].snippet).replace(/[^A-Za-z0-9]/g, ' '));
-    $('#show-lyrics').append($lyric2);
-};
-
-var render3 = function(data){
-    var parser = JSON.parse(data);
-    console.log(parser);
-
-    var rando = Math.floor(Math.random() * (parser.length + 1));
-    var $lyric3 = $('<p>').text(JSON.stringify(parser[rando].snippet).replace(/[^A-Za-z0-9]/g, ' '));
-    $('#show-lyrics').append($lyric3);
-};
-
 
 
 });
