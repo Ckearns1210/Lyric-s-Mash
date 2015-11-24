eb243772d30f7b4dea8d3e492fdcf6d358f3c2d2
jcjohnny <jc2johnny@gmail.com>
2015-11-24 18:29:07 -0500

    cleaner coding

--- controllers/lyric_controller.js
+++ controllers/lyric_controller.js
@@ -13,7 +13,7 @@ var trySendData = function(item,res){
 router.get('/lyric/:search', function(req,res){
     console.log("im here");
 
-  request("http://api.musixmatch.com/ws/1.1/track.search?f_has_lyrics=1&page=1&page_size=15&q_lyrics="+ encodeURI(req.params.search) +"&s_track_rating=desc&apikey=032f5b65b1a0deac1f5f44afc9d548c0", function (error, response, body) {
+  request("http://api.musixmatch.com/ws/1.1/track.search?f_has_lyrics=1&page=1&page_size=1000&q_lyrics="+ encodeURI(req.params.search) + "&s_track_rating=desc&apikey=5ee8f20d45e92ada39d80eb87c848687", function (error, response, body) {
         trySendData(body, res);
     });
 });
@@ -21,14 +21,12 @@ router.get('/lyric/:search', function(req,res){
 router.get('/track/:search', function(req,res){
     console.log("im here");
 
-  request("http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + encodeURI(req.params.search) + "&apikey=032f5b65b1a0deac1f5f44afc9d548c0", function (error, response, body) {
+    request("http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + encodeURI(req.params.search) + "&apikey=5ee8f20d45e92ada39d80eb87c848687", function (error, response, body) {
         trySendData(body, res);
     });
 });
 
 module.exports = router;
-
-
 
 module.exports.controller = function (app) {
 
--- public/js/app.js
+++ public/js/app.js
@@ -3,133 +3,107 @@ $(function(){
 
     $("#search-button").on('click', function(e) {
         e.preventDefault();
-        var searchTerm1 = $('#input-4').val();
-        $.get(('/lyric/' + searchTerm1), render);
-        var searchTerm2 = $('#input-5').val();
-        $.get(('/lyric/' + searchTerm2), render1);
-        var searchTerm3 = $('#input-6').val();
-        $.get(('/lyric/' + searchTerm3), render2);
+        // // var searchTerm3 = $('#input-6').val();
+        // $.get('/lyric/' + searchTerm3, render2,'json');
+        // // var searchTerm2 = $('#input-5').val();
+        // $.get('/lyric/' + searchTerm2, render1 , 'json');
+        // // var searchTerm1 = $('#input-4').val();
+        // $.get('/lyric/' + searchTerm1, render , 'json');
 
+        $.each($('.inputter'), function () {
+            var $currentItem = $(this);
+            var searchTerm = $currentItem.val();
+            $.get('/lyric/' + searchTerm, render.bind(undefined, searchTerm), 'json');
+
+        });
     });
 
+    var render = function (searchTerm, data, xhr){
+        var _render = render.bind(searchTerm, data);
 
-var render = function (data) {
-    var parser = JSON.parse(data);
-    var songList = parser.message.body.track_list;
-    // console.log(songList[0]);
-    var random = Math.floor(Math.random() * (songList.length + 1));
-    // var $lyric1 = $('<p>').text( JSON.stringify(songList[random]));
+        // verify that message body exists
 
-    var randomTrack = songList[random].track;
+        var songList = data.message.body.track_list;
 
-    var spotifyId = randomTrack.track_spotify_id;
-    var trackId = randomTrack.track_id;
-    console.log(spotifyId);
-    console.log(randomTrack);
-    console.log(trackId);
 
-    $.get(('/track/' + trackId), function(data){
-        var parser = JSON.parse(data);
-        console.log(parser);
+        //shuffled songlist
 
-        var trackBody = parser.message.body.lyrics.lyrics_body;
-        var searchTerm = $('#input-4').val();
-        if (trackBody.indexOf(searchTerm) === -1)  {
-            var tracked = $.get(('/lyric/' + $('#input-4').val()), render);
-        } else {
+        var collection = shuffle(songList);
 
-            findLine = trackBody.split(/\n/g);
-            theIndex = 0;
-            for (i = 0; i < findLine.length; i++){
-                if (findLine[i].indexOf(searchTerm) >=1){
-                    theIndex = i;
-                }
-            }
+        var recursiveIterator = (function _rci(item) {
+            $.get('/track/' + item.track.track_id, function (data) {
+                var trackBody = data.message.body.lyrics.lyrics_body;
+                var re = new RegExp ( '\\b' + searchTerm + '\\b', 'g');
+                var foundLine = trackBody.split(/\n/g).find( function(el){
+                    return el.match(re);
+                });
+                return foundLine ? successfulItems(foundLine) : _rci(collection.pop());
+            }, 'json');
 
-        console.log(trackBody);
-        $('#show-lyrics').append($('<p>').text(findLine[theIndex]));
-        }
+        })(collection.pop());
+        // randomize songlist
+        // iterate over each, execute get track, if true get lyric
+        // returns either true or false
 
-    });
-};
+        // var random = Math.floor(Math.random() * (songList.length + 1));
+        //grab random track
+        // var randomTrack = songList[random].track;
+        //grab that tracks ID to for new api search
+        // var trackId = randomTrack.track_id;
 
-var render1 = function (data) {
-    var parser = JSON.parse(data);
-    var songList = parser.message.body.track_list;
-    // console.log(songList[0]);
-    var random = Math.floor(Math.random() * (songList.length + 1));
-    // var $lyric1 = $('<p>').text( JSON.stringify(songList[random]));
+        // getSongLine(trackId);
+    };
 
-    var randomTrack = songList[random].track;
 
-    var spotifyId = randomTrack.track_spotify_id;
-    var trackId = randomTrack.track_id;
-    console.log(spotifyId);
-    console.log(randomTrack);
-    console.log(trackId);
+    // var getSongLine = function (trackId) {
+    //     $.get(('/track/' + trackId), function(data){
+    //     var parser = JSON.parse(data);
+    //     console.log(parser);
+    //
+    //     var trackBody = parser.message.body.lyrics.lyrics_body;
+    //     var searchTerm = $('#input-4').val();
+    //
+    //     if (trackBody.indexOf(searchTerm) === -1)  {
+    //         var tracked = $.get(('/lyric/' + $('#input-4').val()), render);
+    //     } else {
+    //
+    //     var foundLine = trackBody.split(/\n/g).find( function(el){
+    //             return el.indexOf(searchTerm) >=1 ;
+    //     });
+    //
+    //         /// error handle
+    //
+    //     console.log(trackBody);
+    //     $('#show-lyrics').append($('<p>').text(foundLine));
+    //     }
+    //
+    //     });
+    //
+    // };
 
-    $.get(('/track/' + trackId), function(data){
-        var parser = JSON.parse(data);
-        console.log(parser);
 
-        var trackBody = parser.message.body.lyrics.lyrics_body;
-        var searchTerm = $('#input-5').val();
-        if (trackBody.indexOf(searchTerm) === -1)  {
-            var tracked = $.get(('/lyric/' + $('#input-5').val()), render1);
-        } else {
 
-            findLine = trackBody.split(/\n/g);
-            theIndex = 0;
-            for (i = 0; i < findLine.length; i++){
-                if (findLine[i].indexOf(searchTerm) >=1){
-                    theIndex = i;
-                }
-            }
 
-        console.log(trackBody);
-        $('#show-lyrics').append($('<p>').text(findLine[theIndex]));
-        }
-
-    });
-};
-
-var render2 = function (data) {
-    var parser = JSON.parse(data);
-    var songList = parser.message.body.track_list;
-    // console.log(songList[0]);
-    var random = Math.floor(Math.random() * (songList.length + 1));
-    // var $lyric1 = $('<p>').text( JSON.stringify(songList[random]));
-
-    var randomTrack = songList[random].track;
-
-    var spotifyId = randomTrack.track_spotify_id;
-    var trackId = randomTrack.track_id;
-    console.log(spotifyId);
-    console.log(randomTrack);
-    console.log(trackId);
-
-    $.get(('/track/' + trackId), function(data){
-        var parser = JSON.parse(data);
-        console.log(parser);
-
-        var trackBody = parser.message.body.lyrics.lyrics_body;
-        var searchTerm = $('#input-6').val();
-        if (trackBody.indexOf(searchTerm) === -1)  {
-            var tracked = $.get(('/lyric/' + $('#input-6').val()), render2);
-        } else {
-
-            findLine = trackBody.split(/\n/g);
-            theIndex = 0;
-            for (i = 0; i < findLine.length; i++){
-                if (findLine[i].indexOf(searchTerm) >=1){
-                    theIndex = i;
-                }
-            }
-
-        console.log(trackBody);
-        $('#show-lyrics').append($('<p>').text(findLine[theIndex]));
-        }
-
-    });
-};
 });
+var successfulItems = function(word){
+
+    $('#show-lyrics').append($('<p>').text(word));
+};
+
+function shuffle(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
