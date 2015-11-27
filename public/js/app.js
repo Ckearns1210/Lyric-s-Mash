a580378fa3b8f365465ce85e35d7c8ba51c657ee
Thomas T <thomas.j.truong@gmail.com>
2015-11-27 17:16:49 -0500

    voice done

--- public/js/app.js
+++ public/js/app.js
@@ -1,198 +1,16 @@
-
 $(function(){
 
     $("#search-button").on('click', function(e) {
         e.preventDefault();
-
-        var searchTerm1 = $('#input-4').val();
-        $.get(('/lyric/' + searchTerm1), render);
-        var searchTerm2 = $('#input-5').val();
-        $.get(('/lyric/' + searchTerm2), render1);
-        var searchTerm3 = $('#input-6').val();
-        $.get(('/lyric/' + searchTerm3), render2);
-
-    });
-
-
-var render = function (data) {
-    var parser = JSON.parse(data);
-    var songList = parser.message.body.track_list;
-    // console.log(songList[0]);
-    var random = Math.floor(Math.random() * (songList.length + 1));
-    // var $lyric1 = $('<p>').text( JSON.stringify(songList[random]));
-
-
-    var randomTrack = songList[random].track;
-
-    var spotifyId = randomTrack.track_spotify_id;
-    var trackId = randomTrack.track_id;
-    console.log(spotifyId);
-    console.log(randomTrack);
-    console.log(trackId);
-
-    $.get(('/track/' + trackId), function(data){
-        var parser = JSON.parse(data);
-        console.log(parser);
-
-        var trackBody = parser.message.body.lyrics.lyrics_body;
-        var searchTerm = $('#input-4').val();
-        if (trackBody.indexOf(searchTerm) === -1)  {
-            var tracked = $.get(('/lyric/' + $('#input-4').val()), render);
-        } else {
-
-            findLine = trackBody.split(/\n/g);
-            theIndex = 0;
-            for (i = 0; i < findLine.length; i++){
-                if (findLine[i].indexOf(searchTerm) >=1){
-                    theIndex = i;
-                }
-            }
-
-        console.log(trackBody);
-        $('#show-lyrics').append($('<p>').text(findLine[theIndex]));
-        }
-
-
           $.each($('.inputter'), function () {
             var $currentItem = $(this);
             var searchTerm = $currentItem.val();
             $.get('/lyric/' + searchTerm, render.bind(undefined, searchTerm), 'json');
-
-        });
-    });
-};
-
-var render1 = function (data) {
-    var parser = JSON.parse(data);
-    var songList = parser.message.body.track_list;
-    // console.log(songList[0]);
-    var random = Math.floor(Math.random() * (songList.length + 1));
-    // var $lyric1 = $('<p>').text( JSON.stringify(songList[random]));
-
-    var randomTrack = songList[random].track;
-
-    var spotifyId = randomTrack.track_spotify_id;
-    var trackId = randomTrack.track_id;
-    console.log(spotifyId);
-    console.log(randomTrack);
-    console.log(trackId);
-
-    $.get(('/track/' + trackId), function(data){
-        var parser = JSON.parse(data);
-        console.log(parser);
-
-        var trackBody = parser.message.body.lyrics.lyrics_body;
-        var searchTerm = $('#input-5').val();
-        if (trackBody.indexOf(searchTerm) === -1)  {
-            var tracked = $.get(('/lyric/' + $('#input-5').val()), render1);
-        } else {
-
-            findLine = trackBody.split(/\n/g);
-            theIndex = 0;
-            for (i = 0; i < findLine.length; i++){
-                if (findLine[i].indexOf(searchTerm) >=1){
-                    theIndex = i;
-                }
-            }
-
-          console.log(trackBody);
-          var song = trackBody
-
-          $("#talk").click(function() {
-            console.log('talking')
-            // var msg = new SpeechSynthesisUtterance('bro');
-            // window.speechSynthesis.speak(msg);
-
-            var msg = new SpeechSynthesisUtterance();
-            var voices = window.speechSynthesis.getVoices();
-            msg.voice = voices[3]; // Note: some voices don't support altering params
-            msg.voiceURI = 'native';
-            msg.volume = 1; // 0 to 1
-            msg.rate = 1.2; // 0.1 to 10
-            msg.pitch = 1; //0 to 2
-            msg.text = trackBody;
-            msg.lang = 'en-US';
-
-            msg.onend = function(e) {
-              console.log('Finished in ' + event.elapsedTime + ' seconds.');
-            };
-
-            window.speechSynthesis.speak(msg);
-
-
-          });
-
-
-
-
-
-        $('#show-lyrics').append($('<p>').text(findLine[theIndex]));
-        }
-
-    });
-};
-//<<<<<<< HEAD
-// var renderSongLryics = function(data) {
-//     var parser = JSON.parse(data);
-//     var lyrics = parser.message.body.track_list;
-//     console.log(lyrics);
-//
-// }
-
-var render2 = function (data) {
-    var parser = JSON.parse(data);
-    var songList = parser.message.body.track_list;
-    // console.log(songList[0]);
-    var random = Math.floor(Math.random() * (songList.length + 1));
-    // var $lyric1 = $('<p>').text( JSON.stringify(songList[random]));
-
-    var randomTrack = songList[random].track;
-
-    var spotifyId = randomTrack.track_spotify_id;
-    var trackId = randomTrack.track_id;
-    console.log(spotifyId);
-    console.log(randomTrack);
-    console.log(trackId);
-
-    $.get(('/track/' + trackId), function(data){
-        var parser = JSON.parse(data);
-        console.log(parser);
-
-        var trackBody = parser.message.body.lyrics.lyrics_body;
-        var searchTerm = $('#input-6').val();
-        if (trackBody.indexOf(searchTerm) === -1)  {
-            var tracked = $.get(('/lyric/' + $('#input-6').val()), render2);
-        } else {
-
-            findLine = trackBody.split(/\n/g);
-            theIndex = 0;
-            for (i = 0; i < findLine.length; i++){
-                if (findLine[i].indexOf(searchTerm) >=1){
-                    theIndex = i;
-                }
-            }
-
-
-        console.log(trackBody);
-        $('#show-lyrics').append($('<p>').text(findLine[theIndex]));
-        }
-
-    });
-};
-          $.each($('.inputter'), function () {
-            var $currentItem = $(this);
-            var searchTerm = $currentItem.val();
-            $.get('/lyric/' + searchTerm, render.bind(undefined, searchTerm), 'json');
-
         });
     });
 
-    var render = function (searchTerm, data, xhr){
-        var _render = render.bind(searchTerm, data);
 
-        // verify that message body exists
 
-        var songList = data.message.body.track_list;
     var render = function (searchTerm, data, xhr){
         var _render = render.bind(searchTerm, data);
 
@@ -205,23 +23,11 @@ var render2 = function (data) {
 
         var collection = shuffle(songList);
 
-        //shuffled songlist
-
-        var collection = shuffle(songList);
-
         var recursiveIterator = (function _rci(item) {
             $.get('/track/' + item.track.track_id, function (data) {
                 var trackBody = data.message.body.lyrics.lyrics_body;
-                var re = new RegExp ( '\\b' + searchTerm + '\\b', 'g');
-                var foundLine = trackBody.split(/\n/g).find( function(el){
-                    return el.match(re);
-                });
-                return foundLine ? successfulItems(foundLine) : _rci(collection.pop());
-            }, 'json');
+                console.log(data);
 
-        var recursiveIterator = (function _rci(item) {
-            $.get('/track/' + item.track.track_id, function (data) {
-                var trackBody = data.message.body.lyrics.lyrics_body;
                 var re = new RegExp ( '\\b' + searchTerm + '\\b', 'g');
                 var foundLine = trackBody.split(/\n/g).find( function(el){
                     return el.match(re);
@@ -231,13 +37,36 @@ var render2 = function (data) {
 
         })(collection.pop());
 
+
     };
 });
 
 var successfulItems = function(word){
 
     $('#show-lyrics').append($('<p>').text(word));
+    // this plays the 3 lyrics through Google Voice
+    $("#mash-button").click(function() {
+
+          var msg = new SpeechSynthesisUtterance();
+          var voices = window.speechSynthesis.getVoices();
+          msg.voice = voices[3]; // Note: some voices don't support altering params
+          msg.voiceURI = 'native';
+          msg.volume = 1; // 0 to 1
+          msg.rate = 1; // 0.1 to 10
+          msg.pitch = 1; //0 to 2
+          msg.text = word;
+          msg.lang = 'en-US';
+
+          msg.onend = function(e) {
+            console.log('Finished in ' + event.elapsedTime + ' seconds.');
+          };
+
+          window.speechSynthesis.speak(msg);
+
+
+        });
 };
+
 
 var shuffle = function(array) {
   var m = array.length, t, i;
