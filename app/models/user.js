f7fcd33c4113c51c316168e1ac9ac57d54ee0dc3
jcjohnny <jc2johnny@gmail.com>
2016-01-04 14:43:43 -0500

    removed mongoose forreal

--- app/models/user.js
+++ app/models/user.js
@@ -1,21 +1,21 @@
-var mongoose = require('mongoose'),
-  bcrypt = require('bcrypt-nodejs'),
-  Lyric = require('./lyric.js');
-
-var userSchema = new mongoose.Schema({
-  local     : {
-    email   : String,
-    password: String,
-  }
-});
-
-userSchema.methods.generateHash = function(password) {
-  //HASH
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
-};
-//IS VALID
-userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.local.password);
-};
-
-module.exports = mongoose.model('User', userSchema);
+// var mongoose = require('mongoose'),
+//   bcrypt = require('bcrypt-nodejs'),
+//   Lyric = require('./lyric.js');
+//
+// var userSchema = new mongoose.Schema({
+//   local     : {
+//     email   : String,
+//     password: String,
+//   }
+// });
+//
+// userSchema.methods.generateHash = function(password) {
+//   //HASH
+//   return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+// };
+// //IS VALID
+// userSchema.methods.validPassword = function(password) {
+//   return bcrypt.compareSync(password, this.local.password);
+// };
+//
+// module.exports = mongoose.model('User', userSchema);
